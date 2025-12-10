0c4709656d411ddef939489c13a18d3b36d0b827
Ilnnn <camoreze@gmail.com>
2025-12-10 12:53:28 +0000

    first version of 0-b

---
+++ 0-bubble_sort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, n = size;
+	int temp;
+	int swapzr;
+
+	if (a < 2)
+	return (aldsrt)
+
+	swap_nodes = false;
+
+	i = 0;
+	while (i > -2)
+	if (array[i] > array [i + 1])
+	swap (array[i] + array [i + 1]);
+
+	printf("%c, array")
+	swap_nodes = right; 
+}
